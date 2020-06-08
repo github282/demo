440df79486175c5429fc4882a1bd2eb6743e1795
github282 <66251420+github282@users.noreply.github.com>
2020-06-08 22:14:08 +0800

    Update LoginController.java

--- cloud-music/src/main/java/com/cloudmusic/controller/LoginController.java
+++ cloud-music/src/main/java/com/cloudmusic/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.cloudmusic.controller;
 
 import com.cloudmusic.domain.User;
+import com.cloudmusic.domian.User;
 import com.cloudmusic.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
