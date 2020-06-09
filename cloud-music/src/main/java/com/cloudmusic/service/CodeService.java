5f93173e7d1823b1aac92b5da16d636dd53ef720
github282 <66251420+github282@users.noreply.github.com>
2020-06-09 21:13:42 +0800

    asy

--- cloud-music/src/main/java/com/cloudmusic/service/CodeService.java
+++ cloud-music/src/main/java/com/cloudmusic/service/CodeService.java
@@ -1,12 +1,17 @@
 package com.cloudmusic.service;
 
 import com.cloudmusic.dao.CodeDao;
+import com.cloudmusic.dao.UserDao;
 import com.cloudmusic.domian.Code;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 @Service
 public class CodeService {
@@ -14,12 +19,36 @@ public class CodeService {
     @Autowired
     private CodeDao codeDao;
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private UserDao userDao;
+    @Autowired
+    private JavaMailSender mailSender;
+    private static final String mailFrom = "zhuoj127@163.com";
 
+    @Async
     public void sendCodeMail(String username){
-        //将发送邮件添加到队列
-        rabbitTemplate.convertAndSend("topic_exchange",
-                "info.email",username);
+        //产生6位随机数
+        Random random = new Random();
+        String c = "";
+        for (int i = 0; i < 6; i ++){
+            String s = Integer.toString(random.nextInt(10));
+            c += s;
+        }
+        //产生验证码，有效时间5分钟
+        Code code = new Code(username, c, 300);
+        codeDao.save(code);
+
+        String mailTo = userDao.findEmailByUsername(username);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String expireTime = formatter.format(code.getExpireTime());
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(this.mailFrom);
+        message.setTo(mailTo);
+        message.setSubject("您的Cloud Music账户正在进行密码重置");
+        message.setText("验证码:" + code.getCode() +
+                "\n过期时间:" + expireTime);
+        //发送邮件
+        mailSender.send(message);
     }
 
     public boolean checkCode(String username, String code){
